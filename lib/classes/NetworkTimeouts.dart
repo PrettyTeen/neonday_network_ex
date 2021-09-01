8f102084b5a479057fbd802ae8c06db849286577
Denis <pretty.teen7@gmail.com>
2021-09-01 21:38:38 +0300

    Update NetworkTimeouts.dart

--- lib/classes/NetworkTimeouts.dart
+++ lib/classes/NetworkTimeouts.dart
@@ -2,11 +2,13 @@ part of neonday_network_ex;
 
 class NetworkTimeouts {
   final Duration connection;
+  final Duration response;
   final Duration receiveTotal;
   final Duration idle;
   final Duration closing;
   const NetworkTimeouts({
     this.connection = const Duration(seconds: 6),
+    this.response = const Duration(seconds: 10),
     this.receiveTotal = const Duration(minutes: 2),
     this.idle = const Duration(seconds: 10),
     this.closing = const Duration(seconds: 5),
